04f79abe68c826243bcbcbaa6b20112a238c2e44
sabirov07 <shaxzod4242424@gmail.com>
2023-03-18 16:05:32 +0500

    Initial commit

---
+++ test/eventcdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Eventcdk from '../lib/eventcdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eventcdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Eventcdk.EventcdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
